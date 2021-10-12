8016b45c8e837e204fb0b2518fd648e88b0f282b
Mouhcine1221 <emohssine61@gmail.com>
2021-10-12 17:11:08 +0200

    Beans commit

--- src/main/java/fr/ufrsciencestech/panier/Model/Macedoine.java
+++ src/main/java/fr/ufrsciencestech/panier/Model/Macedoine.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package fr.ufrsciencestech.panier.Model;
+import fr.ufrsciencestech.panier.Model.Fruit;
 
 import java.util.ArrayList;
 
@@ -12,32 +13,38 @@ import java.util.ArrayList;
  * @author me253818
  */
 public class Macedoine implements Fruit{
-    private ArrayList<Fruit> fruits;
+    private ArrayList<Fruit> fruits ;
     private bool seedless;
     
         public Macedoine(Fruit f){
-            this.fruits = new ArrayList<>();
+            this.fruits = new ArrayList<Fruit>();
             this.seedless = true;
-         //   this.fruits.add(f);
+            this.fruits.add(f);
         }
         
         //ajout de fruit
         public ajoute(Fruit f){
             this.fruits.add(f);
         }
-        public String ToString(){
+        
+        public String toString(){
             String res ="";
             for(int i = 0 ; i<fruits.size();i++){
-                Fruit f = fruits(i);
-           
+                    Fruit f = fruits.
+                    res = res + f.toString();
         }
+            return res;
         }
         
         
         public double getprix(){
+            double totale;
             for(int i = 0 ;i<this.fruits.size();i++){
+                Fruit f = (Fruit)fruits.get(i);
+                totale = totale + f.getPrix();
                 
             }
+            return totale;
         }
         
         
